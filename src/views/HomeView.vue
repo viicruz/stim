646434e47d00d06eecac4cb72793a3b11edfaaa8
viicruz <VicttorrHugo100@gmail.com>
2024-04-01 17:05:45 -0300

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+ <main class="bg-slate-800 min-h-screen flex items-center justify-center">
+    <div class="text-center text-white">
+      <h1 class="text-4xl font-bold mb-4">Stim!</h1>
+      <h2 class="text-2xl">The best way to buy games online.</h2>
+    </div>
+  </main>
+</template>
